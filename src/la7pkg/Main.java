06930160e436189f842c6e16c6c7b2c046c2e46a
Kevin <kevin.l.stuible@wmich.edu>
2018-11-26 13:01:17 -0500

    main

--- src/la7pkg/Main.java
+++ src/la7pkg/Main.java
@@ -3,7 +3,16 @@ package la7pkg;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		PriorityQueue<Request> requestQueue = new PriorityQueue<Request>();
+		LinkedList<Course> courses = new LinkedList<Course>();
+		BufferedReader fileIn = new BufferedReader(new FileReader("course.txt"));
+		BufferedReader fileIn1 = new BufferedReader(new FileReader("request.txt"));
+		IController control = new Controller(requestQueue, courses, fileIn, fileIn1);
+		control.readCourseFile();
+		control.readRequestFile();
+		control.processRequests();
+		control.printClassList();
 
 	}
 
